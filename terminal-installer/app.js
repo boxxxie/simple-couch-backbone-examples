1b142ff081daa1f6f15d68ec60606f1535dceaf6
Paul <somethingitalian@gmail.com>
2011-11-04 13:12:38 -0400

    did some comment cleanup

--- terminal-installer/app.js
+++ terminal-installer/app.js
@@ -1,15 +1,6 @@
  var couchapp = require('couchapp'), 
 path = require('path');
 
-/*
- *
- * <PPaul> http://localhost:5984/campaigns/_all_docs?include_docs=true
-<PPaul> the docs are below that node, though
-<_sorensen_> can you make _all_docs a param?
-<PPaul> http://localhost:5984/campaigns?_all_docs&include_docs=true
-<_sorensen_> why dont you just rewrite localhost/campaigns/ to give you all models by default?
- * */
-
 ddoc = { _id:'_design/app', 
 	 rewrites :[ 
 	     {from:"/api", to:'../../'},
@@ -18,8 +9,6 @@ ddoc = { _id:'_design/app',
 	     {from:"/db/*", to:'../../*'},
 	     {from:"/*", to:'*'}]};
 
-
-//ddoc.views.lib = couchapp.loadFiles('./common');
 couchapp.loadAttachments(ddoc, path.join(__dirname, 'attachments'));
 
 module.exports = ddoc;
