172f7aed5cbeb4aabf0737788f632f8f260c1a30
Yunbo Lee <yblee85@gmail.com>
2011-11-18 13:39:24 -0500

    a

--- terminal-installer/attachments/Inventory.js
+++ terminal-installer/attachments/Inventory.js
@@ -8,8 +8,8 @@ var Inventory = couchDoc.extend(
 	     apply_taxes:{/*tax1:false, tax2:false, tax3:false, exemption:false*/},
 	     category:"unknown",
 	     description:"unknown",
-	     location:{},
-	     price:{}
+	     location:{/*company:company_id, group:group_id, store:store_id*/},
+	     price:{/*selling_price:0.00, standard_price:0.00*/}
 	 }
      }
   });
