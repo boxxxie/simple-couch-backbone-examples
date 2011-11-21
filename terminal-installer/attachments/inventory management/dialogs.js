f83f74178f231558ce9ac341a270fff26b472261
Paul <somethingitalian@gmail.com>
2011-11-21 10:11:13 -0500

    the new version of mustache isn't used because ich.js has an older one embedded. fetch a patch from the git issue for an upgrade. inventory management is working a lot better, need to finish the dialog code for converting a array of strings into an object path. in the inventory management code, need to set the focus to the upc input box.

--- terminal-installer/attachments/inventory management/dialogs.js
+++ terminal-installer/attachments/inventory management/dialogs.js
@@ -1,5 +1,28 @@
 var genericErrorClass = "ui-state-error";
 var genericHighlightClass = "ui-state-highlight";
+
+function jsPather(pathStr){
+    //converts js obj notation into a path array
+    return pathStr
+	.replace(/\[/g,'.')
+	.replace(/\]/g,'')
+	.split(".");
+};
+function assignFromPath(obj,travel,assignVal){
+    //walks a path defined by an array of fields, assigns a value to the last field walked
+    if(_.isEmpty(travel)){
+	obj = assignVal;
+	return obj;
+    }
+    if(!obj){
+	return null;
+    }
+    var prop = _.first(travel);
+    obj[prop] = assignFromPath(obj[prop],_.rest(travel),assignVal);
+    return obj;
+};
+//TODO: make the assignFromPath function create the obj structure for the path if it doesn't exist
+
 function PostValidator($node,tips,validationResults) {
     function clearErrorsAndTips(){	
 	tips.html("");
@@ -19,8 +42,13 @@ function PostValidator($node,tips,validationResults) {
     };
     
     clearErrorsAndTips();
-    var invalidsWithEmptyMessages = _.filter(validationResults, function(validationResult){return validationResult.isInvalid && _.isEmpty(validationResult.errMsg);});
-    var foundInvalidFieldWithMessage = _.filter(validationResults, function(validationResult){return validationResult.isInvalid && !_.isEmpty(validationResult.errMsg);});
+    var invalidsWithEmptyMessages = 
+	_.filter(validationResults, 
+		 function(validationResult){return validationResult.isInvalid && _.isEmpty(validationResult.errMsg);});
+    var foundInvalidFieldWithMessage = 
+	_.filter(validationResults, 
+		 function(validationResult){return validationResult.isInvalid && !_.isEmpty(validationResult.errMsg);});
+
     if(!_.isEmpty(foundInvalidFieldWithMessage) && 
        _.isEmpty(invalidsWithEmptyMessages)){
 	var tipMessage = ich.tips_TMP({tips: _.map(foundInvalidFieldWithMessage,function(invalid){return invalid.errMsg;})});
@@ -33,81 +61,26 @@ function PostValidator($node,tips,validationResults) {
     }	 
 };
 
-function CompanyCreateDialog (attachTo,options){
+function InventoryItemCreateDialog (attachTo,options){
     _.extend(options,{clearOnExit:true, isCreate:true});
-    CompanyInputDialog(attachTo,options);
+    InventoryItemInputDialog(attachTo,options);
 };
-function CompanyModifyDialog (attachTo,options){
-    CompanyInputDialog(attachTo,options);
+
+function InventoryItemModifyDialog (attachTo,options){
+    InventoryItemInputDialog(attachTo,options);
 };
-function GroupCreateDialog (attachTo,options){
-    _.extend(options,{clearOnExit:true, isCreate:true});
-    GroupInputDialog(attachTo,options);
-};
-function GroupModifyDialog (attachTo,options){
-    GroupInputDialog(attachTo,options);
-};
-function StoreCreateDialog (attachTo,options){
-    _.extend(options,{clearOnExit:true, isCreate:true});
-    StoreInputDialog(attachTo,options);
-};
-function StoreModifyDialog (attachTo,options){
-    StoreInputDialog(attachTo,options);
-};
-function TerminalCreateDialog (attachTo,options){
-    _.extend(options,{clearOnExit:true, isCreate:true});
-    TerminalInputDialog(attachTo,options);
-};
-function TerminalModifyDialog (attachTo,options){
-    TerminalInputDialog(attachTo,options);
-};
-function CompanyInputDialog (attachTo,options) {
+
+function InventoryItemInputDialog (attachTo,options) {
     // a workaround for a flaw in the demo system (http://dev.jqueryui.com/ticket/4375), ignore!
     $("#dialog:ui-dialog").dialog( "destroy" );
     var tips = $(".validateTips");
     var d = $("#dialog-form");
 
-    var user = d.find("#user"),
-    password = d.find("#password"),
-    companyName = d.find("#company-name"),
-    firstname = d.find("#contact\\.firstname"),
-    lastname = d.find("#contact\\.lastname"),
-    website = d.find("#contact\\.website"),
-    email = d.find("#contact\\.email"),
-    phone = d.find("#contact\\.phone"),
-    street0 = d.find("#address\\.street0"),
-    street1 = d.find("#address\\.street1"),
-    street2 = d.find("#address\\.street2"),
-    city = d.find("#address\\.city"),
-    province = d.find("#address\\.province"),
-    country = d.find("#address\\.country"),
-    postalcode = d.find("#address\\.postalcode"),
-    operationalname = d.find("#operationalname"),
-
-    
-    allFields = $([])
-    	.add(user)
-	.add(companyName)
-	.add(firstname)
-	.add(lastname)
-	.add(website)
-	.add(email)
-	.add(phone)
-	.add(street0)
-	.add(street1)
-	.add(street2)
-	.add(city)
-	.add(province)
-	.add(country)
-	.add(postalcode)
-	.add(operationalname)
-	.add(password);
-
     d.dialog(
 	{
 	    autoOpen: false,
-	    height: 900,
-	    width: 500,
+	    height: 510,
+	    width: 300,
 	    modal: true,
 	    close: function() {
 		if(options.clearOnExit) {
@@ -116,40 +89,27 @@ function CompanyInputDialog (attachTo,options) {
 	    },
 	    buttons: {			 
 		"Submit" : function() {
-		    
-		    var newCompanyData = 
-			{user:user.val(),
-			 password:password.val(),
-			 contact:{firstname : firstname.val(),
-				  lastname : lastname.val(),
-				  website : website.val(),
-				  email : email.val(),
-				  phone : phone.val()},
-			 address:{street0:street0.val(),
-				  street1:street1.val(),
-				  street2:street2.val(),
-				  city:city.val(),
-				  country:country.val(),
-				  province:province.val(),
-				  postalcode:postalcode.val()},
-			 operationalname:operationalname.val(),
-			 companyName:companyName.val()};
+		    var allFields = d.find('[var]');
+		    var newInventoryItemData = {};
 
-		    var newCompanyData_w_options = _.clone(newCompanyData);
+		    var newInventoryItemData_w_options = _.clone(newInventoryItemData);
 
 		    if(options.isCreate) {
-			_.extend(newCompanyData, {creationdate:new Date()});
-			_.extend(newCompanyData_w_options, {isCreate:options.isCreate});
+			_.extend(newInventoryItemData, {creationdate:new Date()});
+			_.extend(newInventoryItemData_w_options, {isCreate:options.isCreate});
 		    }
-
-		    var validationResults = options.validator(newCompanyData_w_options);
+		    
+		    var validator = options.validator;
+		    if(validator){
+			var validationResults = validator(newInventoryItemData_w_options);
+		    }
 
 		    var passedValidation;
 		    (_.isEmpty(validationResults))?passedValidation=true:passedValidation=false;
 		    allFields.removeClass(genericErrorClass);
 
 		    if (passedValidation) {
-			options.success(newCompanyData);
+			options.success(newInventoryItemData);
 			allFields.val("");
 			d.dialog("close");
 		    }
@@ -168,317 +128,3 @@ function CompanyInputDialog (attachTo,options) {
 				       d.dialog("open");
 				   });
 };
-function GroupInputDialog (attachTo,options) {
-    // a workaround for a flaw in the demo system (http://dev.jqueryui.com/ticket/4375), ignore!
-    $("#dialog:ui-dialog").dialog("destroy");
-    var d = $("#dialog-form");
-    var tips = $(".validateTips");
-    var groupName = d.find("#group-name"),
-    user = d.find("#user"),
-    password = d.find("#password"),
-    firstname = d.find("#contact\\.firstname"),
-    lastname = d.find("#contact\\.lastname"),
-    website = d.find("#contact\\.website"),
-    email = d.find("#contact\\.email"),
-    phone = d.find("#contact\\.phone"),
-    street0 = d.find("#address\\.street0"),
-    street1 = d.find("#address\\.street1"),
-    street2 = d.find("#address\\.street2"),
-    city = d.find("#address\\.city"),
-    province = d.find("#address\\.province"),
-    country = d.find("#address\\.country"),
-    postalcode = d.find("#address\\.postalcode"),
-
-
-    allFields = $([])
-	.add(groupName)
-   	.add(user)
-	.add(firstname)
-	.add(lastname)
-	.add(website)
-	.add(email)
-	.add(phone)
-	.add(street0)
-	.add(street1)
-	.add(street2)
-	.add(city)
-	.add(province)
-	.add(country)
-	.add(postalcode)
-	.add(password);
-    
-    var dialogOptions = _.extend(
-	{autoOpen: false,
-	 height: 900,
-	 width: 500,
-	 modal: true,
-	 close: function() {
-	     if(options.clearOnExit) {
-		 allFields.val("").removeClass( "ui-state-error" );
-	     }
-	 },
-	 buttons: {
-	     "Submit": function() {
-		 var newGroupData = 
-		     {user:user.val(),
-		      password:password.val(),
-		      contact:{firstname : firstname.val(),
-			       lastname : lastname.val(),
-			       website : website.val(),
-			       email : email.val(),
-			       phone : phone.val()},
-		      address:{street0:street0.val(),
-			       street1:street1.val(),
-			       street2:street2.val(),
-			       city:city.val(),
-			       country:country.val(),
-			       province:province.val(),
-			       postalcode:postalcode.val()},
-		      groupName:groupName.val()};
-		 var newGroupData_w_options = _.clone(newGroupData);
-
-		 if(options.isCreate) {
-			_.extend(newGroupData, {creationdate:new Date()});
-			_.extend(newGroupData_w_options, {isCreate:options.isCreate});
-		 }
-		 var validationResults = options.validator(newGroupData_w_options);
-
-		 var passedValidation;
-		 (_.isEmpty(validationResults))?passedValidation=true:passedValidation=false;
-		 allFields.removeClass(genericErrorClass);
-
-		 if(passedValidation) {
-			 options.success(newGroupData);
-			 allFields.val("");
-		     d.dialog("close");
-		 }
-		 else{
-		     PostValidator(d,tips,validationResults);
-		 }
-	     },		
-	     Cancel: function() {
-		 d.dialog("close");
-	     }
-	 }
-	},_.clone(options));
-    
-    d.dialog(dialogOptions);
-    $("#"+attachTo).button().click(function() {
-				       d.dialog( "open" );
-				   });
-};
-function StoreInputDialog (attachTo,options) {
-    // a workaround for a flaw in the demo system (http://dev.jqueryui.com/ticket/4375), ignore!
-    $("#dialog:ui-dialog").dialog( "destroy" );
-    var d = $("#dialog-form");
-    var tips = $(".validateTips");
-
-    var user = d.find("#user"),
-    password = d.find("#password"),
-    storeName = d.find("#store-name"),
-    storeNum = d.find("#store-num"),
-    firstname = d.find("#contact\\.firstname"),
-    lastname = d.find("#contact\\.lastname"),
-    website = d.find("#contact\\.website"),
-    email = d.find("#contact\\.email"),
-    phone = d.find("#contact\\.phone"),
-    street0 = d.find("#address\\.street0"),
-    street1 = d.find("#address\\.street1"),
-    street2 = d.find("#address\\.street2"),
-    city = d.find("#address\\.city"),
-    province = d.find("#address\\.province"),
-    country = d.find("#address\\.country"),
-    postalcode = d.find("#address\\.postalcode"),
-    
-   
-    allFields = $([])
-	.add(user)
-	.add(storeName)
-	.add(storeNum)
-	.add(firstname)
-	.add(lastname)
-	.add(website)
-	.add(email)
-	.add(phone)
-	.add(street0)
-	.add(street1)
-	.add(street2)
- 	.add(city)
-	.add(province)
-	.add(country)
-	.add(postalcode)
-	.add(password);
-    
-    d.dialog(
-	{
-	    autoOpen: false,
-	    height: 900,
-	    width: 500,
-	    modal: true,
-	    buttons: {
-		"Submit": function() {
-
-		    var newStoreData = 
-			{user:user.val(),
-			    password:password.val(),
-			    contact:{firstname : firstname.val(),
-				     lastname : lastname.val(),
-				     website : website.val(),
-				     email : email.val(),
-				     phone : phone.val()},
-			    address:{street0:street0.val(),
-				     street1:street1.val(),
-				     street2:street2.val(),
-				     city:city.val(),
-				     country:country.val(),
-				     province:province.val(),
-				     postalcode:postalcode.val()},
-			    storeName:storeName.val(),
-			    number:storeNum.val()};
-
-		    var newStoreData_w_options = _.clone(newStoreData);
-
-		    var validationResults = options.validator(newStoreData_w_options);
-
-		    var passedValidation;
-		    (_.isEmpty(validationResults))?passedValidation=true:passedValidation=false;
-		    allFields.removeClass(genericErrorClass);
-
-		    if(passedValidation) {
-			options.success(newStoreData);
-			allFields.val("");
-			d.dialog("close");
-		    }
-		    else{
-			PostValidator(d,tips,validationResults);
-		    }  
-		},
-		Cancel: function() {
-		    d.dialog("close");
-		}
-	    },
-	    close: function() {
-		if(options.clearOnExit) {
-		    allFields.val("").removeClass(genericErrorClass);
-		}
-	    }
-	});
-
-    $("#"+attachTo).button().click(function() {
-				       d.dialog("open");
-				   });
-};
-function TerminalInputDialog (attachTo,options) {
-    // a workaround for a flaw in the demo system (http://dev.jqueryui.com/ticket/4375), ignore!
-    $( "#dialog:ui-dialog" ).dialog( "destroy" );
-    var d = $("#dialog-form");
-    var label = d.find("#terminal-id"),
-    areaCode = d.find("#areaCode"),
-    postalCode = d.find("#postalCode"),
-    countryCode = d.find("#countryCode"),
-    cityCode = d.find("#cityCode"),
-    storeCode = d.find("#storeCode"),
-    companyCode = d.find("#companyCode"),
-    centrallycontrolmenus = d.find("#centrallycontrolmenus"),
-    usingautomatedpayment = d.find("#usingautomatedpayment"),
-    usingmobqredits = d.find("#usingmobqredits"),
-
-    allFields = $([])
-	.add(label)
-	.add(areaCode)
-	.add(postalCode)
-	.add(countryCode)
-	.add(cityCode)
-	.add(storeCode)
-	.add(companyCode)
-	.add(centrallycontrolmenus)
-	.add(usingautomatedpayment)
-	.add(usingmobqredits);
-
-    var tips = $(".validateTips");
-
-    d.dialog(
-	{autoOpen: false,
-	 height: 600,
-	 width: 500,
-	 modal: true,
-	 close: function() {
-	     if(options.clearOnExit) {
-		 allFields.val("").removeClass(genericClassError);
-		 allFields.filter("input:checked").attr("checked",false);
-	     }
-	 },
-	 buttons: {
-	     "Submit": function() {
-		 var newTerminalData = {
-			     terminal_label:label.val(),
-			     areaCode:areaCode.val(),
-			     postalCode:postalCode.val(),
-			     countryCode:countryCode.val(),
-			     cityCode:cityCode.val(),
-			     storeCode:storeCode.val(),
-			     companyCode:companyCode.val(),
-			     usingautomatedpayment:usingautomatedpayment.is(":checked"),
-			     usingmobqredits:usingmobqredits.is(":checked"),
-			     centrallycontrolmenus:centrallycontrolmenus.is(":checked"),
-			     usingautomatedpayment:usingautomatedpayment.is(":checked")
-			     
-			 };
-
-		var newTerminalData_w_options = _.clone(newTerminalData);
-		 if(options.isCreate) {
-			_.extend(newTerminalData, {creationdate:new Date(), installed:false});
-			_.extend(newTerminalData_w_options, {isCreate:options.isCreate});
-		 }
-
-
-		 var validationResults = options.validator(newTerminalData_w_options);
-
-		 var passedValidation;
-		 (_.isEmpty(validationResults))?passedValidation=true:passedValidation=false;
-		 allFields.removeClass(genericErrorClass);
-
-		 if(passedValidation) {
-		     options.success(newTerminalData);
-		     allFields.val("");
-		     d.dialog("close");
-		 }
-		 else{
-		     PostValidator(d,tips,validationResults);
-		 }  
-	     },
-	     Cancel: function() {
-		 d.dialog("close");
-	     }
-	 }
-	});
-	
-	
-    $("#"+attachTo).button().click(function() {
-				       d.dialog("open");
-				   });
-};
-
-function quickViewDialog (html,options) {
-    var form = $(html).filter('fieldset');
-    var d = $("#dialog-quickView");    	
-    d.html(form);
-    d.find('input').attr('disabled',true);
-    var dialogOptions = _.extend(
-	{autoOpen: false,
-	 height: 400,
-	 width: 500,
-	 modal: true,
-	 buttons: {
-	     Cancel: function() {
-		 d.dialog('destroy');
-	     }
-	 },
-	 close: function() {
-	     d.dialog('destroy');
-	 }
-	},_.clone(options));
-    d.dialog(dialogOptions);
-    d.dialog("open");
-};
-
--- terminal-installer/attachments/inventory management/inventory management.js
+++ terminal-installer/attachments/inventory management/inventory management.js
@@ -1,69 +1,49 @@
-function addInventory(){
-    var d = $("#dialog-form");
-    var company = d.find("#location\\.company"),
-    group = d.find("#location\\.group"),
-    store = d.find("#location\\.store"),
-    tax1 = d.find("#apply_taxes\\.tax1"),
-    tax2 = d.find("#apply_taxes\\.tax2"),
-    tax3 = d.find("#apply_taxes\\.tax3"),
-    exemption = d.find("#apply_taxes\\.exemption"),
-    selling_price = d.find("#price\\.selling_price"),
-    standard_price = d.find("#price\\.standard_price"),
-    upc= d.find("#upc");
-        
-    var inventoryInfo = {_id:upc.val(),upccode:upc.val(),
-    					 location : {company:company.val(), group:group.val(), store:store.val()},
-    					 apply_taxes : {tax1:tax1.is(':checked'), tax2:tax2.is(':checked'), tax3:tax3.is(':checked'), exemption:exemption.is(':checked')},
-    					 price : {selling_price:selling_price.val(), standard_price:standard_price.val()}    					 
-    					};
-
-	//console.log("item : " + item);
-    var urlBase = window.location.protocol + "//" + window.location.hostname + ":" +window.location.port + "/";
-    var dbName = "inventory";
-    var db_inventory = couchDoc.extend({urlRoot:urlBase + dbName});
-    var inventoryToAdd = new db_inventory(inventoryInfo);
-
-    //on the success of the install of the first terminal, install the second terminal.
-    //this way we only have one success message.
-    //it may be possible to do this with triggers, TameJS, or monads, in a more elegant way.
-    inventoryToAdd.save({},
-			       {success:function(){
-			       	alert("saved successfully");
-				    },
-				    error:function(){
-				    	console.log("fail");
-				    	//inventoryToAdd.update({success:function(){alert("update successfully")}});
-				    }});
-
-    //terminal.installed = true;
-    //company.save();
-    
-}
-
-
-
 function doc_setup() {
     var urlBase = window.location.protocol + "//" + window.location.hostname + ":" +window.location.port + "/";
     var db = 'inventory';
     var InventoryItem = couchDoc.extend({urlRoot:urlBase+db});
+
+    function addItem(){
+	return {success: function(resp){
+		    var itemToAdd = new InventoryItem(resp);
+		    itemToAdd.save({success:function(){
+					window.location.href ='#upc/'+resp._id;
+				    }});
+		}
+	       };
+    };
+
+
+    function addCompany(collection){
+	return {success: function(resp){
+		    collection.create(resp);},
+		validator : function(resp) {
+		    return validateCompany(resp,null);}
+	       };};
+    function editCompany(company){
+	return {success:function(resp){
+		    company.save(resp);},
+		validator : function(resp) {
+		    return validateCompany(resp,company.toJSON());}
+	       };};
 
     var AppRouter = new 
     (Backbone.Router.extend(
 	 {
 	     routes: {
 		 "":"inventoryManagementHome",
-		 "inventory/:upc": "addmodifyInventory"  
+		 "upc/":"inventoryManagementHome",
+		 "upc/:upc": "addmodifyInventory"  
 	     },
 	     inventoryManagementHome:function(){
 		 console.log("inventoryManagementHome");
-		 var mainHTML = ich.inventoryManagementHome_TMP();
-		 $("body").html(mainHTML);
-		 var upcInputHTML = ich.upcInput_TMP();
-		 $("upc").html(upcInputHTML);
-		 $("upc").find("input").change(function(a){
-						   window.location.href ='#/inventory/'+$(this).val();
-						   console.log( $(this).val());
-						   console.log(a);});
+		 var html = ich.inventoryManagementHome_TMP();
+		 $("body").html(html);
+		 $("#upc")
+		     .change(function(){
+				 var upc = $(this).val();
+				 window.location.href ='#upc/'+upc;		
+			     });
 	     },
 	     addmodifyInventory:function(upc){
 		 console.log("addmodifyInventory: " + upc);
@@ -76,42 +56,48 @@ function doc_setup() {
 	     _.bindAll(view, 'renderManagementPage','renderModifyPage');
 	     AppRouter.bind('route:inventoryManagementHome', function(){
 				console.log('inventoryView:route:inventoryManagementHome');
-				view.el= _.first($("inv_item"));
+				view.el= _.first($("main"));
 				view.renderManagementPage();});
 	     AppRouter.bind('route:addmodifyInventory', function(upc){
 				//fetch model based on upc code
-				view.model = new InventoryItem({_id:upc, upc:upc});
-				view.model.fetch();
+				view.model = new InventoryItem({_id:upc});
 				view.model.bind('change',function(){view.renderModifyPage(upc);});
-				console.log('companiesView:route:modifyInventory');
-				view.renderModifyPage(upc);});
+				view.model.bind('not_found',function(){view.renderAddPage(upc);});
+				view.model.fetch({error:function(a,b,c){
+						      console.log("couldn't load model");
+						      view.model.trigger('not_found');
+						  }});
+				console.log('InventoryView:route:modifyInventory');});
 	     
 	 },
 	 renderManagementPage:function(){
 	     var view = this;
 	     if(view.model){
-		 var html = ich.inventoryForm_TMP({item:view.model.toJSON()});
-		 $(this.el).html(html);
+		 $(this.el).html("");
 	     }
 	     console.log("InventoryView renderManagementPage");
 	     return this;
 	 },
 	 renderModifyPage:function(upc){
 	     var view = this;
-	     if(view.model){
-		 var html = ich.inventoryForm_TMP({item:view.model.toJSON()});
-		 $(this.el).html(html);
-	     }
+	     var html = ich.inventoryForm_TMP(view.model.toJSON());
+	     $(html).find('input').attr('disabled',true);
+	     $(this.el).html(html);
 	     console.log("InventoryView renderModifyPage " + upc);
+	     return this;
+	 },
+	 renderAddPage:function(upc){
+	     var view = this;
+	     var html = ich.inventoryAddPage_TMP({createButtonLabel:"create new inventory item",_id:upc});
+	     $(this.el).html(html);
+	     $("#dialog-hook").html(ich.inventoryInputDialog_TMP({title:"Add "+upc+" to the Inventory",location:{},applyTaxes:{},price:{}}));
+	     InventoryItemCreateDialog("create-thing",addItem());
+	     console.log("InventoryView renderAddPage " + upc);
 	     return this;
 	 }
 	});
 
-    var InvItemDisplay = new InventoryView(
-//	{
-//	    el:_.first($("inv_item"))
-//	});
-	);
+    var InvItemDisplay = new InventoryView();
     Backbone.history.start();
 
 }
