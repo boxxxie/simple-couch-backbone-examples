c5ea8fed78bb2d5287fded47b47e0c20a6f55a96
Yunbo Lee <yblee85@gmail.com>
2011-11-25 14:45:15 -0500

    intent

--- terminal-installer/attachments/layered management/report.js
+++ terminal-installer/attachments/layered management/report.js
@@ -121,7 +121,7 @@ function doc_setup() {
 	     var transactionsView = cdb.view('reporting','id_type_date');
 	     var transaction_db = cdb.db('transactions');
 	     var view = this;
-		var param = getReportParam();
+	     var param = getReportParam();
 	     var today = _.first(Date.today().toArray(),3);
 	     var yesterday = _.first(Date.today().addDays(-1).toArray(),3);
 
@@ -184,7 +184,7 @@ function doc_setup() {
 	 },
 	 renderGroupsTable: function() {
 	     var view = this;
-		var param = getGroupsTableParam();
+	     var param = getGroupsTableParam();
 	     _.extend(param, {breadCrumb:"Company : " + ReportData.company.operationalname});
 	     var html = ich.groupsTabel_TMP(param);
 	     $("body").html(html);
@@ -193,7 +193,7 @@ function doc_setup() {
 	 },
 	 renderStoresTable: function(group_id) {
 	     var view = this;
-		var param = getStoresTableParam(group_id);
+	     var param = getStoresTableParam(group_id);
 	     _.extend(param, {breadCrumb:"Company : " + ReportData.company.operationalname});
 	     var html = ich.storesTabel_TMP(param);
 	     $("body").html(html);
@@ -203,7 +203,7 @@ function doc_setup() {
 	 },
 	 renderTerminalsTable : function(store_id) {
 	     var view = this;
-		var param = getTerminalsTableParam(store_id);
+	     var param = getTerminalsTableParam(store_id);
 	     _.extend(param, {breadCrumb:"Company : " + ReportData.company.operationalname});
 	     var html = ich.terminalsTabel_TMP(param);
 	     $("body").html(html);
@@ -234,7 +234,7 @@ function doc_setup() {
 	 },
 	 renderGroupReport: function() {
 	     var view = this;
-		var param = getReportParam();
+	     var param = getReportParam();
 	     var html = ich.groupManagementPage_TMP(param);
 	     $("body").html(html);
 	     console.log("groupReportView renderGroupReport");
@@ -242,7 +242,7 @@ function doc_setup() {
 	 },
 	 renderStoresTable : function() {
 	     var view = this;
-		var param = getStoresTableParam();
+	     var param = getStoresTableParam();
 	     _.extend(param, {breadCrumb:"Company : " + ReportData.companyName});
 	     var html = ich.storesTabel_TMP(param);
 	     $("body").html(html);
@@ -251,7 +251,7 @@ function doc_setup() {
 	 },
 	 renderTerminalsTable:function(store_id) {
 	     var view = this;
-		var param = getTerminalsTableParam(store_id);
+	     var param = getTerminalsTableParam(store_id);
 	     _.extend(param, {breadCrumb:"Company : " + ReportData.companyName});
 	     var html = ich.terminalsTabel_TMP(param);
 	     $("body").html(html);
@@ -279,7 +279,7 @@ function doc_setup() {
 	 renderStoreReport: function() {
 	     var view = this;
 	     
-		var param = getReportParam();
+	     var param = getReportParam();
 	     var html = ich.storeManagementPage_TMP(param);
 	     $("body").html(html);
 	     console.log("storeReportView renderStoreReport");
@@ -287,7 +287,7 @@ function doc_setup() {
 	 },
 	 renderTerminalsTable: function() {
 	     var view = this;
-		var param = getTerminalsTableParam();
+	     var param = getTerminalsTableParam();
 	     _.extend(param, {breadCrumb:"Company : " + ReportData.companyName});
 	     var html = ich.terminalsTabel_TMP(param);
 	     $("body").html(html);
@@ -350,194 +350,194 @@ function login() {
 };
 
 function getReportParam() {
-	if(!_.isEmpty(ReportData.company)){
-		var company = ReportData.company;
-	     var groups = company.hierarchy.groups; 
-	     var stores = _(groups).chain().map(function(group) {return group.stores;}).flatten().value();
-	     
-	     var numGroups = _.size(groups);
-	     var numStores = _.reduce(groups, function(sum, group){ return sum + _.size(group.stores); }, 0);
-	     var numTerminals = _.reduce(stores, function(sum, store){ return sum + _.size(store.terminals); }, 0);
-	     return {sales:{yesterdaysales:"100",mtdsales:"100",ytdsales:"100"},
-			   numberOfGroups:numGroups,
-			   numberOfStores:numStores,
-			   numberOfTerminals:numTerminals,
-			   company_id:company._id,
-			   startPage:'companyReport',
-			   breadCrumb:"company : " + company.operationalname
-			  };
-	} else if(!_.isEmpty(ReportData.group)) {
-		var group = ReportData.group; 
-	     var stores = group.stores;
-	     
-	     var numStores = _.size(stores);
-	     var numTerminals = _.reduce(stores, function(sum, store){ return sum + _.size(store.terminals); }, 0);
-	     return  {sales:{yesterdaysales:"100",mtdsales:"100",ytdsales:"100"},
-			   numberOfStores:numStores,
-			   numberOfTerminals:numTerminals,
-			   startPage:"groupReport",
-			   breadCrumb:"company : " + ReportData.companyName + " , group : " + group.groupName
-			  };
-	} else if(!_.isEmpty(ReportData.store)) {
-		var store = ReportData.store;
-	     var terminals = store.terminals;
-	     var numTerminals = _.size(terminals);
-	     return  {sales:{yesterdaysales:"100",mtdsales:"100",ytdsales:"100"},
-			   numberOfTerminals:numTerminals,
-			   startPage:"storeReport",
-			   breadCrumb:"company : " + ReportData.companyName + 
-			   " , group : " + ReportData.groupName +
-			   " , store : " + store.storeName
-			  };
-	}	
+    if(!_.isEmpty(ReportData.company)){
+	var company = ReportData.company;
+	var groups = company.hierarchy.groups; 
+	var stores = _(groups).chain().map(function(group) {return group.stores;}).flatten().value();
+	
+	var numGroups = _.size(groups);
+	var numStores = _.reduce(groups, function(sum, group){ return sum + _.size(group.stores); }, 0);
+	var numTerminals = _.reduce(stores, function(sum, store){ return sum + _.size(store.terminals); }, 0);
+	return {sales:{yesterdaysales:"100",mtdsales:"100",ytdsales:"100"},
+		numberOfGroups:numGroups,
+		numberOfStores:numStores,
+		numberOfTerminals:numTerminals,
+		company_id:company._id,
+		startPage:'companyReport',
+		breadCrumb:"company : " + company.operationalname
+	       };
+    } else if(!_.isEmpty(ReportData.group)) {
+	var group = ReportData.group; 
+	var stores = group.stores;
+	
+	var numStores = _.size(stores);
+	var numTerminals = _.reduce(stores, function(sum, store){ return sum + _.size(store.terminals); }, 0);
+	return  {sales:{yesterdaysales:"100",mtdsales:"100",ytdsales:"100"},
+		 numberOfStores:numStores,
+		 numberOfTerminals:numTerminals,
+		 startPage:"groupReport",
+		 breadCrumb:"company : " + ReportData.companyName + " , group : " + group.groupName
+		};
+    } else if(!_.isEmpty(ReportData.store)) {
+	var store = ReportData.store;
+	var terminals = store.terminals;
+	var numTerminals = _.size(terminals);
+	return  {sales:{yesterdaysales:"100",mtdsales:"100",ytdsales:"100"},
+		 numberOfTerminals:numTerminals,
+		 startPage:"storeReport",
+		 breadCrumb:"company : " + ReportData.companyName + 
+		 " , group : " + ReportData.groupName +
+		 " , store : " + store.storeName
+		};
+    }	
 };
 
 function getGroupsTableParam() {
-	var company = ReportData.company;
-     var groups = company.hierarchy.groups; 
-     
-     return {list: _.map(groups, function(group) {
-				  var numberOfStores = _.size(group.stores);
-				  var numberOfTerminals = _.reduce(group.stores, function(sum, store){ return sum + _.size(store.terminals); }, 0);;
-				  var sales={yesterdaysales:"100",mtdsales:"100",ytdsales:"100"};
-				  return {operationalname:company.operationalname,
-					  groupName:group.groupName,
-					  group_id:group.group_id,
-					  numberOfStores:numberOfStores,
-					  numberOfTerminals:numberOfTerminals,
-					  sales:sales,
-					  startPage:"companyReport"};
-			      })};
+    var company = ReportData.company;
+    var groups = company.hierarchy.groups; 
+    
+    return {list: _.map(groups, function(group) {
+			    var numberOfStores = _.size(group.stores);
+			    var numberOfTerminals = _.reduce(group.stores, function(sum, store){ return sum + _.size(store.terminals); }, 0);;
+			    var sales={yesterdaysales:"100",mtdsales:"100",ytdsales:"100"};
+			    return {operationalname:company.operationalname,
+				    groupName:group.groupName,
+				    group_id:group.group_id,
+				    numberOfStores:numberOfStores,
+				    numberOfTerminals:numberOfTerminals,
+				    sales:sales,
+				    startPage:"companyReport"};
+			})};
 };
 
 function getStoresTableParam(group_id) {
-	if(!_.isEmpty(ReportData.company)) {
-		var company = ReportData.company;
-	     var groups;
-	     
-	     if(_.isEmpty(group_id)){
-		 groups = company.hierarchy.groups;
-	     } else {
-		 groups = _.filter(company.hierarchy.groups, function(group){ return group.group_id==group_id;});
-	     } 
-	     
-	     var stores = _(groups).chain().map(function(group) {
-						    return _.map(group.stores, function(store){
-								     return _.extend(_.clone(store), {groupName:group.groupName});
-								 }); 
-						}).flatten().value();
-	     
-	     return {list: _.map(stores, function(store) {
-					  var numberOfTerminals = _.size(store.terminals);
-					  var sales={yesterdaysales:"100",mtdsales:"100",ytdsales:"100"};
-					  return {operationalname:company.operationalname,
-						  groupName:store.groupName,
-						  store_id:store.store_id,
-						  storeName:store.storeName,
-						  storeNumber:store.number,
-						  numberOfTerminals:numberOfTerminals,
-						  sales:sales,
-						  startPage:"companyReport"};
-				      })};
-	} else if(!_.isEmpty(ReportData.group)) {
-		var group = ReportData.group;
-	     var stores = group.stores;
-	     return {list: _.map(stores, function(store) {
-					  var numberOfTerminals = _.size(store.terminals);
-					  var sales={yesterdaysales:"100",mtdsales:"100",ytdsales:"100"};
-					  return {operationalname:ReportData.companyName,
-						  groupName:group.groupName,
-						  store_id:store.store_id,
-						  storeName:store.storeName,
-						  storeNumber:store.number,
-						  numberOfTerminals:numberOfTerminals,
-						  sales:sales,
-						  startPage:"groupReport"};
-				      })};
-	}
+    if(!_.isEmpty(ReportData.company)) {
+	var company = ReportData.company;
+	var groups;
+	
+	if(_.isEmpty(group_id)){
+	    groups = company.hierarchy.groups;
+	} else {
+	    groups = _.filter(company.hierarchy.groups, function(group){ return group.group_id==group_id;});
+	} 
+	
+	var stores = _(groups).chain().map(function(group) {
+					       return _.map(group.stores, function(store){
+								return _.extend(_.clone(store), {groupName:group.groupName});
+							    }); 
+					   }).flatten().value();
+	
+	return {list: _.map(stores, function(store) {
+				var numberOfTerminals = _.size(store.terminals);
+				var sales={yesterdaysales:"100",mtdsales:"100",ytdsales:"100"};
+				return {operationalname:company.operationalname,
+					groupName:store.groupName,
+					store_id:store.store_id,
+					storeName:store.storeName,
+					storeNumber:store.number,
+					numberOfTerminals:numberOfTerminals,
+					sales:sales,
+					startPage:"companyReport"};
+			    })};
+    } else if(!_.isEmpty(ReportData.group)) {
+	var group = ReportData.group;
+	var stores = group.stores;
+	return {list: _.map(stores, function(store) {
+				var numberOfTerminals = _.size(store.terminals);
+				var sales={yesterdaysales:"100",mtdsales:"100",ytdsales:"100"};
+				return {operationalname:ReportData.companyName,
+					groupName:group.groupName,
+					store_id:store.store_id,
+					storeName:store.storeName,
+					storeNumber:store.number,
+					numberOfTerminals:numberOfTerminals,
+					sales:sales,
+					startPage:"groupReport"};
+			    })};
+    }
 };
 
 function getTerminalsTableParam(store_id) {
-	if(!_.isEmpty(ReportData.company)){
-		var company = ReportData.company;
-	     var groups;
-	     var stores;
-	     groups = company.hierarchy.groups;
-	     if(_.isEmpty(store_id)){
-		 stores = _(groups).chain().map(function(group) {
-						    return _.map(group.stores, function(store){
-								     return _.extend(_.clone(store), {groupName:group.groupName});
-								 }); 
-						}).flatten().value();
-	     } else {
-		 stores = _(groups).chain().map(function(group) {
-						    return _.map(group.stores, function(store){
-								     return _.extend(_.clone(store), {groupName:group.groupName});
-								 }); 
-						}).flatten().filter(function(store){return store.store_id==store_id;}).value();
-	     }
-	     var terminals = _(stores).chain()
-		 .map(function(store){
-			  return _.map(store.terminals, function(terminal){
-					   return _.extend(_.clone(terminal), 
-							   {groupName:store.groupName, 
-							    storeName:store.storeName, 
-							    storeNumber:store.number 
-							    });
-				       });})
-		 .flatten()
-		 .value();
-	     return {list: _.map(terminals, function(terminal) {
-					  var sales={yesterdaysales:"100",mtdsales:"100",ytdsales:"100"};
-					  return {operationalname:company.operationalname,
-						  groupName:terminal.groupName,
-						  storeName:terminal.storeName,
-						  storeNumber:terminal.storeNumber,
-						  terminalName:terminal.terminal_label,
-						  sales:sales,
-						  startPage:"companyReport"};
-				      })};
-	} else if(!_.isEmpty(ReportData.group)) {
-		var group = ReportData.group;
-	     var stores;
-	     if(_.isEmpty(store_id)){
-		 stores = group.stores;
-	     } else {
-		 stores = _.filter(group.stores, function(store){return store.store_id ==store_id;});
-	     }
-	     var terminals = _(stores).chain()
-		 .map(function(store){
-			  return _.map(store.terminals, function(terminal){
-					   return _.extend(_.clone(terminal), 
-							   {groupName:group.groupName, 
-							    storeName:store.storeName, 
-							    storeNumber:store.number});});})
-		 .flatten()
-		 .value();
-
-	     return {list: _.map(terminals, function(terminal) {
-					  var sales={yesterdaysales:"100",mtdsales:"100",ytdsales:"100"};
-					  return {operationalname:ReportData.companyName,
-						  groupName:terminal.groupName,
-						  storeName:terminal.storeName,
-						  storeNumber:terminal.storeNumber,
-						  terminalName:terminal.terminal_label,
-						  sales:sales,
-						  startPage:"groupReport"};
-				      })};
-	} else if(!_.isEmpty(ReportData.store)) {
-		var store = ReportData.store;	
-	     
-	     var terminals = store.terminals;
-	     return {list: _.map(terminals, function(terminal) {
-					  var sales={yesterdaysales:"100",mtdsales:"100",ytdsales:"100"};
-					  return {operationalname:ReportData.companyName,
-						  groupName:ReportData.groupName,
-						  storeName:store.storeName,
-						  storeNumber:store.number,
-						  terminalName:terminal.terminal_label,
-						  sales:sales,
-						  startPage:"storeReport"};
-				      })};
+    if(!_.isEmpty(ReportData.company)){
+	var company = ReportData.company;
+	var groups;
+	var stores;
+	groups = company.hierarchy.groups;
+	if(_.isEmpty(store_id)){
+	    stores = _(groups).chain().map(function(group) {
+					       return _.map(group.stores, function(store){
+								return _.extend(_.clone(store), {groupName:group.groupName});
+							    }); 
+					   }).flatten().value();
+	} else {
+	    stores = _(groups).chain().map(function(group) {
+					       return _.map(group.stores, function(store){
+								return _.extend(_.clone(store), {groupName:group.groupName});
+							    }); 
+					   }).flatten().filter(function(store){return store.store_id==store_id;}).value();
 	}
+	var terminals = _(stores).chain()
+	    .map(function(store){
+		     return _.map(store.terminals, function(terminal){
+				      return _.extend(_.clone(terminal), 
+						      {groupName:store.groupName, 
+						       storeName:store.storeName, 
+						       storeNumber:store.number 
+						      });
+				  });})
+	    .flatten()
+	    .value();
+	return {list: _.map(terminals, function(terminal) {
+				var sales={yesterdaysales:"100",mtdsales:"100",ytdsales:"100"};
+				return {operationalname:company.operationalname,
+					groupName:terminal.groupName,
+					storeName:terminal.storeName,
+					storeNumber:terminal.storeNumber,
+					terminalName:terminal.terminal_label,
+					sales:sales,
+					startPage:"companyReport"};
+			    })};
+    } else if(!_.isEmpty(ReportData.group)) {
+	var group = ReportData.group;
+	var stores;
+	if(_.isEmpty(store_id)){
+	    stores = group.stores;
+	} else {
+	    stores = _.filter(group.stores, function(store){return store.store_id ==store_id;});
+	}
+	var terminals = _(stores).chain()
+	    .map(function(store){
+		     return _.map(store.terminals, function(terminal){
+				      return _.extend(_.clone(terminal), 
+						      {groupName:group.groupName, 
+						       storeName:store.storeName, 
+						       storeNumber:store.number});});})
+	    .flatten()
+	    .value();
+
+	return {list: _.map(terminals, function(terminal) {
+				var sales={yesterdaysales:"100",mtdsales:"100",ytdsales:"100"};
+				return {operationalname:ReportData.companyName,
+					groupName:terminal.groupName,
+					storeName:terminal.storeName,
+					storeNumber:terminal.storeNumber,
+					terminalName:terminal.terminal_label,
+					sales:sales,
+					startPage:"groupReport"};
+			    })};
+    } else if(!_.isEmpty(ReportData.store)) {
+	var store = ReportData.store;	
+	
+	var terminals = store.terminals;
+	return {list: _.map(terminals, function(terminal) {
+				var sales={yesterdaysales:"100",mtdsales:"100",ytdsales:"100"};
+				return {operationalname:ReportData.companyName,
+					groupName:ReportData.groupName,
+					storeName:store.storeName,
+					storeNumber:store.number,
+					terminalName:terminal.terminal_label,
+					sales:sales,
+					startPage:"storeReport"};
+			    })};
+    }
 };
