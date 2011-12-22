bb2b145666861da54452678558afe208124251fb
Paul <somethingitalian@gmail.com>
2011-12-22 16:02:44 -0500

    changed the layout for the menu item button edit dialog

--- terminal-installer/attachments/layered management/menuMenusRouter.js
+++ terminal-installer/attachments/layered management/menuMenusRouter.js
@@ -149,8 +149,10 @@ function saveEditMenu() {
 
     var newButtonItemData = varFormGrabber(editDialog);
 
+    console.log(newButtonItemData.display.description);
     newButtonItemData.display.description = _(newButtonItemData.display.description).toArray();
-	      
+    console.log(newButtonItemData.display.description);
+
     console.log("newButtonItemData");
     console.log(newButtonItemData);
 
