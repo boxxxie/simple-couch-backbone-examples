0f7f62332c977eca721707f5a535283798691e7d
Yunbo Lee <yblee85@gmail.com>
2012-01-17 12:30:19 -0500

    for merge

--- terminal-installer/attachments/company management/docRdy.js
+++ terminal-installer/attachments/company management/docRdy.js
@@ -23,6 +23,28 @@ function jodaDateFormatter(dateString){
 function jodaDatePartFormatter(dateString){
     return datePartFormatter(jodaDateParser(dateString));
 }
+
+function getBrowserName() {
+var browserName = "";
+
+var ua = navigator.userAgent.toLowerCase();
+if ( ua.indexOf( "opera" ) != -1 ) {
+browserName = "opera";
+} else if ( ua.indexOf( "msie" ) != -1 ) {
+browserName = "msie";
+} else if ( ua.indexOf( "safari" ) != -1 ) {
+browserName = "safari";
+} else if ( ua.indexOf( "mozilla" ) != -1 ) {
+if ( ua.indexOf( "firefox" ) != -1 ) {
+browserName = "firefox";
+} else {
+browserName = "mozilla";
+}
+}
+
+return browserName;
+}; 
+
 $(document)
     .ready(function() {
 	       //this is for IE7
