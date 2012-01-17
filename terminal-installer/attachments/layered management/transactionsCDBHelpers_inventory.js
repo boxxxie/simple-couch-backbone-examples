8f0bfbc0899c4312bfe526919c53fb0b372b0c97
Yunbo Lee <yblee85@gmail.com>
2012-01-17 13:04:52 -0500

    fix inventory lable

--- terminal-installer/attachments/layered management/transactionsCDBHelpers_inventory.js
+++ terminal-installer/attachments/layered management/transactionsCDBHelpers_inventory.js
@@ -133,7 +133,10 @@ function inventoryTotalsRangeFetcher_F(id){
 				 var labels = scan.label.split('-');
 				 var upc = _.str.trim(_.first(labels));
 				 var description =  _.str.trim(_.second(labels));
-				 return _.extend({upc: upc, label:description},scan);
+
+				 var data = _.extend({},scan);
+				 //return _.extend({upc: upc, label:description},scan);
+				 return _.extend(data,{upc: upc, label:description});
 			     });
 
 		    var scan_sales_totals = _.reduce(scan_sales_list,_.addPropertiesTogether,{});
