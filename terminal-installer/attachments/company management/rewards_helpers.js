f1fdc518ed7c653621c135ffc48202798595e106
Paul <somethingitalian@gmail.com>
2012-01-17 11:22:31 -0500

    removed comments from the rewards.js file removed the .save() in the set_default_rewards function

--- terminal-installer/attachments/company management/rewards_helpers.js
+++ terminal-installer/attachments/company management/rewards_helpers.js
@@ -1,18 +1,17 @@
 function fetch_company_rewards(companyID){
     
-    //var terminal_menu = new Menu({_id:companyID});
-    var terminal_rewards = new Rewards({_id:companyID}); //new Menu({_id:companyID});
+
+    var terminal_rewards = new Rewards({_id:companyID});
     
     return function(callback){
 	terminal_rewards.fetch(
 	    {success:function(model){
-		 //return use the menu set up by the company.		     
-		 //callback(_.selectKeys(model.toJSON(),['menuButtons','menuButtonHeaders']));
 		 callback(false, model);
 	     },
 	     error:function(model){
-		 //return an empty menu
+		 //return and save an empty Rewards profile to the rewards db
 		 model.set_default_rewards(companyID);
+		 model.save();
 		 callback(true, model);
 	     }});
     };
---
+++ terminal-installer/push.sh
@@ -0,0 +1,1 @@
+couchapp push ~/Dropbox/work/rt7/db/simple-couch-backbone-examples/terminal-installer/app.js http://localhost:5984/install
