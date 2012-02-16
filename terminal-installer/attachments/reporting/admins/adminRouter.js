235924b8815f0cae36a0c34f95986b35517c0899
Yunbo Lee <yblee85@gmail.com>
2012-02-16 10:57:50 -0500

    adminrouter.js indenting stuff

--- terminal-installer/attachments/reporting/admins/adminRouter.js
+++ terminal-installer/attachments/reporting/admins/adminRouter.js
@@ -27,50 +27,54 @@ var adminRouter =
               }
 	     }));
 
-var menuAdminUsersView = Backbone.View.extend({
-						  initialize:function() {
-						      console.log(this.collection);
-						      (this.collection).on("change",this._renderTable,this);
-						      this._renderTable(this.collection);
-						  },
-						  _renderTable:function(collection) {
-						      var list = collection.toJSON();
-						      $("#usersInfoTable").html(ich.adminUsersInfotable_TMP({list:list}));
-						      _.each(list,function(item){
-						          $("#edit-"+item._id).button().click(function(){
-						              alert("edit");
-						          });
-						      
-						      $("#del-"+item._id).button().click(function(){
-                                      alert("del");
-                                  });
-						      });
-						      $("#addusers").button().click(function(){
-											function getDefaultData(ReportData) {
-											    if(ReportData.company) {
-												return {company_id:ReportData.company._id,
-													companyName:ReportData.company.companyName};
-											    } else if(ReportData.group) {
-												return {company_id:ReportData.company_id,
-													companyName:ReportData.companyName,
-													group_id:ReportData.group.group_id,
-													groupName:ReportData.group.groupName};
-											    } else if(ReportData.store) {
-												return {company_id:ReportData.company_id,
-													companyName:ReportData.companyName,
-													group_id:ReportData.group_id,
-													groupName:ReportData.groupName,
-													store_id:ReportData.store.store_id,
-													storeName:ReportData.store.storeName,
-													storeNumber:ReportData.store.number};
-											    } else {
-												return undefined;
-											    }
-											};
-											quickInputUserInfoDialog(ich.inputUserInfo_TMP({}),{title:"Add New User"
-																	    ,collection:collection
-																	    ,defaultData:getDefaultData(ReportData)});
-										    });
-						  }
-						  
-					      });
+var menuAdminUsersView = 
+    Backbone.View.extend({
+			     initialize:function() {
+				 console.log(this.collection);
+				 (this.collection).on("change",this._renderTable,this);
+				 this._renderTable(this.collection);
+			     },
+			     _renderTable:function(collection) {
+				 var list = collection.toJSON();
+				 $("#usersInfoTable").html(ich.adminUsersInfotable_TMP({list:list}));
+				 _.each(list,function(item){
+					    $("#edit-"+item._id).button().click(function(){
+										    alert("edit");
+										});
+					    
+					    $("#del-"+item._id).button().click(function(){
+										   alert("del");
+									       });
+					});
+				 $("#addusers")
+				     .button()
+				     .click(function(){
+						function getDefaultData(ReportData) {
+						    if(ReportData.company) {
+							return {company_id:ReportData.company._id,
+								companyName:ReportData.company.companyName};
+						    } else if(ReportData.group) {
+							return {company_id:ReportData.company_id,
+								companyName:ReportData.companyName,
+								group_id:ReportData.group.group_id,
+								groupName:ReportData.group.groupName};
+						    } else if(ReportData.store) {
+							return {company_id:ReportData.company_id,
+								companyName:ReportData.companyName,
+								group_id:ReportData.group_id,
+								groupName:ReportData.groupName,
+								store_id:ReportData.store.store_id,
+								storeName:ReportData.store.storeName,
+								storeNumber:ReportData.store.number};
+						    } else {
+							return undefined;
+						    }
+						};
+						quickInputUserInfoDialog(ich.inputUserInfo_TMP({})
+								         ,{title:"Add New User"
+									   ,collection:collection
+									   ,defaultData:getDefaultData(ReportData)});
+					    });
+			     }
+			     
+			 });
