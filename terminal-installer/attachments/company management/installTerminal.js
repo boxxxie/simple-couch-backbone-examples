e100a2faddd013ebcfcc17c226265bc9244e37a1
Paul <somethingitalian@gmail.com>
2012-03-22 17:17:19 -0400

    made it so a user needs to be logged in in order to set up a terminal for installation (we need their user ID/name to be attached to terminals)

--- terminal-installer/attachments/company management/installTerminal.js
+++ terminal-installer/attachments/company management/installTerminal.js
@@ -13,55 +13,69 @@ function installTerminal(companyID,groupID,storeID,terminalID){
 	alert("The terminal could not be installed");
 	return;
     }
-    function terminal_install_properties(company,group,store,terminal,sample_terminal){
-	return {terminal_id:terminal.terminal_id,
-		terminal_label:terminal.terminal_label,
-		store_id:store.store_id,
-		store_label:store.storeName,
-		group_id:group.group_id,
-		group_label:group.groupName,
-		company_id:company.get('_id'),
-		company_label:company.get('companyName'),
-		location:_.selectKeys(terminal,["postalCode","areaCode","storeCode","companyCode","cityCode","countryCode"]),
-		creationDate:terminal.creationdate,
-		receipt_id:terminal.receipt_id,
-		
-		printers:sample_terminal.printers,
-		departments:sample_terminal.departments,
-		menuButtonHeaders:sample_terminal.menuButtonHeaders,
-		menuButtons:sample_terminal.menuButtons,
-		scales:sample_terminal.scales,
-		showMobQRedits:terminal.usingmobqredits,
-		locally_modifiable:false, //!terminal.centrallycontrolmenus,
-		automatedPayment:terminal.usingautomatedpayment,
-		
-		creditPayment:sample_terminal.creditPayment,
-		debitPayment:sample_terminal.debitPayment,
-		mobilePayment:sample_terminal.mobilePayment,
-		paymentGateway:sample_terminal.paymentGateway,
-		
-		receiptHeaders:sample_terminal.receiptHeaders,
-		cardserver:sample_terminal.cardserver,
-		cardserverport:sample_terminal.cardserverport,
-		cardterminalid:sample_terminal.cardterminalid,
-		connectiontimeout:sample_terminal.connectiontimeout,
-		store_address:{
-		    country:store.address.country,
-		    state_prov:store.address.province,
-		    city:store.address.city,
-		    zip_postal:store.address.postalcode,
-		    street:[store.address.street0,store.address.street1],
-		    phones:[store.contact.phone],
-		    emails:[store.contact.email]
-		},
-		store_number:store.number
-	       };
+    function get_terminal_install_properties(company,group,store,terminal,sample_terminal){
+	return function(callback){
+	    $.couch.session(
+		{
+		    success:function(user){
+			var terminal_data=
+			    {
+				terminal_id:terminal.terminal_id,
+				terminal_label:terminal.terminal_label,
+				store_id:store.store_id,
+				store_label:store.storeName,
+				group_id:group.group_id,
+				group_label:group.groupName,
+				company_id:company.get('_id'),
+				company_label:company.get('companyName'),
+				location:_.selectKeys(terminal,["postalCode","areaCode","storeCode","companyCode","cityCode","countryCode"]),
+				creationDate:terminal.creationdate,
+				receipt_id:terminal.receipt_id,
+
+				printers:sample_terminal.printers,
+				departments:sample_terminal.departments,
+				menuButtonHeaders:sample_terminal.menuButtonHeaders,
+				menuButtons:sample_terminal.menuButtons,
+				scales:sample_terminal.scales,
+				showMobQRedits:terminal.usingmobqredits,
+				locally_modifiable:false, //!terminal.centrallycontrolmenus,
+				automatedPayment:terminal.usingautomatedpayment,
+
+				creditPayment:sample_terminal.creditPayment,
+				debitPayment:sample_terminal.debitPayment,
+				mobilePayment:sample_terminal.mobilePayment,
+				paymentGateway:sample_terminal.paymentGateway,
+
+				receiptHeaders:sample_terminal.receiptHeaders,
+				cardserver:sample_terminal.cardserver,
+				cardserverport:sample_terminal.cardserverport,
+				cardterminalid:sample_terminal.cardterminalid,
+				connectiontimeout:sample_terminal.connectiontimeout,
+				store_address:{
+				    country:store.address.country,
+				    state_prov:store.address.province,
+				    city:store.address.city,
+				    zip_postal:store.address.postalcode,
+				    street:[store.address.street0,store.address.street1],
+				    phones:[store.contact.phone],
+				    emails:[store.contact.email]
+				},
+				store_number:store.number,
+				creation_user:user.userCtx.name
+			    };
+			callback(terminal_data);
+		    },
+		    error:function(){
+			alert("please log in to install terminals");
+		    }
+		});
+	}
     }
-    
+
 
     if(terminal.installed){
 	alert("The terminal has been installed already");
-	return;	
+	return;
     }
 
     var urlBase = window.location.protocol + "//" + window.location.hostname + ":" +window.location.port + "/";
@@ -72,13 +86,13 @@ function installTerminal(companyID,groupID,storeID,terminalID){
 	{success:function(model){
 	     //use the menu set up by the company for this terminal installation.
 	     var sample_terminal_with_menus = _.extend({},sample_terminal,_.selectKeys(model.toJSON(),['menuButtons','menuButtonHeaders']));
-	     var new_terminal_properties = terminal_install_properties(company,group,store,terminal,sample_terminal_with_menus);
-	     installRT7_and_CORP_terminals(new_terminal_properties);
+	     get_terminal_install_properties(company,group,store,terminal,sample_terminal_with_menus)
+	     (installRT7_and_CORP_terminals);
 	 },
 	 error:function(){
 	     //install the terminal with the default sample terminal menu (possibly empty)
-	     var new_terminal_properties = terminal_install_properties(company,group,store,terminal,sample_terminal);
-	     installRT7_and_CORP_terminals(new_terminal_properties);
+	     get_terminal_install_properties(company,group,store,terminal,sample_terminal)
+	     (installRT7_and_CORP_terminals);
 	 }});
 
     //FIXME: use async.js waterfall
@@ -97,6 +111,6 @@ function installTerminal(companyID,groupID,storeID,terminalID){
 						    alert("The terminal is now ready to be Installed");
 						    terminal.installed = true;
 						    company.save();
-						}});}});  
-    }   
+						}});}});
+    }
 }
