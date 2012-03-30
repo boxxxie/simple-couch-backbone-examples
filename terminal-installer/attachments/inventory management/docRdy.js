0c6ffc85a5786c73859722335ec45846ba88fbc0
Yunbo Lee <yblee85@gmail.com>
2012-03-30 18:26:57 -0400

    delete stuff

--- terminal-installer/attachments/inventory management/docRdy.js
+++
@@ -1,9 +0,0 @@
-$(document)
-    .ready(function() {
-        //this is for IE7
-        if(_.isUndefined(window.console)){
-           console = {log:function(){/*do nothing*/}};
-           }
-           
-	       doc_setup();
-	   });
